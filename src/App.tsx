67873077be6b3f95b9863791a799014707d7bd7b
ivanrussui <fenomen_ny@mail.ru>
2024-03-17 20:03:59 +0300

    feat Роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Counter} from './Counter/Counter';
 import {CounterAdvanced} from './CounterAdvanced/CounterAdvanced';
@@ -7,9 +8,25 @@ import {CounterAdvanced2in1} from './CounterAdvanced2in1/CounterAdvanced2in1';
 const App: FC = () => {
     return (
         <div className="App">
-            <Counter/>
-            <CounterAdvanced/>
-            <CounterAdvanced2in1/>
+            <h1 className={'Title'}>Choose your Counter</h1>
+            <NavLink to={'/counter'}
+                     className={({ isActive}) => isActive ? "Active" : "NavLink"}>
+                Counter
+            </NavLink>
+            <NavLink to={'/counter-advanced'}
+                     className={({ isActive}) => isActive ? "Active" : "NavLink"}>
+                Counter Advanced with Local Storage
+            </NavLink>
+            <NavLink to={'/counter-advanced-2-in-1'}
+                     className={({ isActive}) => isActive ? "Active" : "NavLink"}>
+                Counter Advanced 2 in 1 with Local Storage
+            </NavLink>
+            <Routes>
+                <Route path={'/counter'} element={<Counter/>}/>
+                <Route path={'/counter-advanced'} element={<CounterAdvanced/>}/>
+                <Route path={'/counter-advanced-2-in-1'} element={<CounterAdvanced2in1/>}/>
+                <Route path={'/*'} element={<Navigate to={'/'}/>}/>
+            </Routes>
         </div>
     );
 };
